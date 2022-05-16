aa348197a8072f928b86af432d399cc598f04714
Mohamed Ayman <muhamed.ayman.fahmy@gmail.com>
2022-05-16 16:05:53 +0200

    fix Img Error
    just Change The Link :)

--- netflix-app/src/components/Navbar.jsx
+++ netflix-app/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
           <div className={classes.item}>
             {/* <img src="https://www.pngwing.com/en/free-png-kmjrb"  alt="avatar" */}
             <img
-              src="../../assets/Images/admin.png"
+              src="https://media-exp1.licdn.com/dms/image/C5603AQGjUbgWwxw7-g/profile-displayphoto-shrink_800_800/0/1649322747808?e=1658361600&v=beta&t=cfPmTdv7u2cc3W_rSX5o4SdKteYOqH9aOCCdBJXJud4"
               alt="avatar"
               className={classes.avatar}
             />
--- netflix-app/src/pages/Single.jsx
+++ netflix-app/src/pages/Single.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import Sidebar from "./../components/Sidebar";
+import Navbar from "./../components/Navbar";
+import "../styles/single.css";
 const Single = () => {
-  return <div className="home">Single Page</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <h1 className="title">INFORMATION</h1>
+            <div className="item">
+              <img
+                src="https://media-exp1.licdn.com/dms/image/C5603AQGjUbgWwxw7-g/profile-displayphoto-shrink_800_800/0/1649322747808?e=1658361600&v=beta&t=cfPmTdv7u2cc3W_rSX5o4SdKteYOqH9aOCCdBJXJud4"
+                alt="profile img"
+                className="itemImg"
+              />
+              Ditails
+            </div>
+          </div>
+          <div className="Right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
